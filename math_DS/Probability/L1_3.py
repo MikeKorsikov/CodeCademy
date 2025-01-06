502ad413bc4009c60c538cceff6195bf0e5ebc47
MikeKorsikov <nikolay.korsikov@gmail.com>
2025-01-06 11:44:45 +0100

    "Added probability formulas"

---
+++ math_DS/Probability/L1_3.py
@@ -0,0 +1,12 @@
+# Conditional probability
+
+# Independant events
+# P(A|B) = P(A)
+# P(B|A) = P(B)
+
+# probability that two [dependent] events occur simultaneously 
+# P(A and B) = P(A) * P(B|A)
+
+# probability that two [independent] events occur simultaneously 
+# P(A and B) = P(A) * P(B)
+
