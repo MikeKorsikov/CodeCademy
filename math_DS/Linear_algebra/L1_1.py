971ef544c501062cf874de11de2cd6d2bc07e6fc
MikeKorsikov <nikolay.korsikov@gmail.com>
2025-01-14 22:04:49 +0100

    started matrices

--- math_DS/Linear_algebra/L1_1.py
+++ math_DS/Linear_algebra/L1_1.py
@@ -1,1 +1,19 @@
 # vectors
+# dot product
+# matrices 
+# Need to review vectors and matrices again out of CodeCademy
+
+import numpy as np
+
+# Given vectors
+vector_1 = np.array([-2,-6,2,3])
+vector_2 = np.array([4,1,-3,8])
+vector_3 = np.array([5,-7,9,0])
+
+matrix = np.column_stack((vector_1, vector_2, vector_3))
+print(matrix)
+
+print(matrix.shape)
+
+# print third column
+print(matrix[:,2])
