6eb1803cb3557d9c6249a6749956f25e72abe0dd
MikeKorsikov <nikolay.korsikov@gmail.com>
2025-01-19 14:45:27 +0100

    Started Master statistics (variables)

---
+++ Master_stats_with_Python/1_variables/L1_1.py
@@ -0,0 +1,14 @@
+# Variables types
+
+# Import pandas with alias
+import pandas as pd
+
+# Import dataset as a Pandas dataframe
+movies = pd.read_csv("Master_stats_with_Python/1_variables/netflix_movies.csv", index_col=0)
+
+# View the first five rows of the dataframe
+
+
+# Set the correct value for rating_variable_type
+rating_variable_type = None
+print(rating_variable_type)
