54101c782d9b537768ee0494c5e1d8fd4973881c
MikeKorsikov <nikolay.korsikov@gmail.com>
2025-01-21 19:59:00 +0100

    Added census data

--- Master_stats_with_Python/L1_variables/L1_5.py
+++ Master_stats_with_Python/L1_variables/L1_5.py
@@ -4,5 +4,5 @@
 import pandas as pd
 
 # Read in the census dataframe
-census = pd.read_csv('census_data.csv', index_col=0)
+census = pd.read_csv('Codecademy\Master_stats_with_Python\L1_variables\census_data.csv', index_col=0)
 
