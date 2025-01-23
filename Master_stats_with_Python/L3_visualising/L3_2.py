c42f424209d43f4ce05c561d54c0160c6fdff860
MikeKorsikov <nikolay.korsikov@gmail.com>
2025-01-23 10:50:48 +0100

    Completed hist project

--- Master_stats_with_Python/L3_visualising/L3_2.py
+++ Master_stats_with_Python/L3_visualising/L3_2.py
@@ -1,4 +1,5 @@
 # Histogram project
+
 import numpy as np
 from matplotlib import pyplot as plt
 
@@ -6,9 +7,21 @@ from matplotlib import pyplot as plt
 in_bloom = np.loadtxt(open("in-bloom.csv"), delimiter=",")
 flights = np.loadtxt(open("flights.csv"), delimiter=",")
 
-# Plot the histograms
+# 
+plt.figure(1)
+plt.subplot(211)
 
+# Plot the histograms (step 1)
+plt.hist(flights, range = (0, 365), bins = 365)
+plt.xlabel("Day of the year")
+plt.ylabel("Number of flights")
+plt.title("Acadia")
 
+#Plot the histograms (step 2)
+plt.subplot(212)
+plt.hist(in_bloom, range = (0, 365), bins=365)
+plt.ylabel("Number of flowers")
+plt.xlabel("Day of the year")
 
-
+plt.tight_layout() # prevent overlapping
 plt.show()
