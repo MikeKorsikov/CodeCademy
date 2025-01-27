dc3391bef071aea5264e520f87b0e6aecddf1e8f
MikeKorsikov <nikolay.korsikov@gmail.com>
2025-01-27 20:52:48 +0100

    Add water usage

--- Master_stats_with_Python/L5_visualising_categorical/L5_4.py
+++ Master_stats_with_Python/L5_visualising_categorical/L5_4.py
@@ -1,1 +1,7 @@
 # pie charts
+
+import matplotlib.pyplot as plt
+import pandas as pd
+
+water_usage = pd.read_csv("Codecademy\Master_stats_with_Python\L5_visualising_categorical\water_usage.csv")
+print(water_usage.head())
