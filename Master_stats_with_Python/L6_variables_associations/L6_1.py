1cea9aa0e6d0b4daa15b0d726a1b06617290e45c
MikeKorsikov <nikolay.korsikov@gmail.com>
2025-01-28 19:30:12 +0100

    Started variables association

---
+++ Master_stats_with_Python/L6_variables_associations/L6_1.py
@@ -0,0 +1,9 @@
+import numpy as np
+import pandas as pd
+
+students = pd.read_csv('Codecademy\Master_stats_with_Python\L6_variables_associations\students.csv')
+
+#print the first five rows of students:
+
+
+#separate out scores for students who live in urban and rural locations:
