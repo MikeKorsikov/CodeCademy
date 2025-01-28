94d56400d1da3944c14b6330864fb487e3e74f55
MikeKorsikov <106008283+MikeKorsikov@users.noreply.github.com>
2025-01-28 12:27:06 +0100

    Update L5_4.py

--- Master_stats_with_Python/L5_visualising_categorical/L5_4.py
+++ Master_stats_with_Python/L5_visualising_categorical/L5_4.py
@@ -5,3 +5,11 @@ import pandas as pd
 
 water_usage = pd.read_csv("Codecademy\Master_stats_with_Python\L5_visualising_categorical\water_usage.csv")
 print(water_usage.head())
+
+wedge_sizes = water_usage['prop']
+pie_labels = water_usage['water_source']
+
+plt.pie(wedge_sizes, labels = pie_labels)
+plt.axis('equal')
+plt.title('Distribution of House Water Usage')
+plt.show()
