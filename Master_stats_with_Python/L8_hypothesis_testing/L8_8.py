da7059076de70ce9094a12e3dcc01d24433e6e9b
MikeKorsikov <nikolay.korsikov@gmail.com>
2025-02-02 18:55:49 +0100

    Started project on associations

--- Master_stats_with_Python/L8_hypothesis_testing/L8_8.py
+++ Master_stats_with_Python/L8_hypothesis_testing/L8_8.py
@@ -1,17 +1,22 @@
 # Chi-Square Test
+# to understand whether the outcomes of two categorical are associated
+
 import pandas as pd
 from scipy.stats import chi2_contingency
 
 # read in and print data
-ants = pd.read_csv("ants_grade.csv")
+ants = pd.read_csv("Codecademy\Master_stats_with_Python\L8_hypothesis_testing\\ants_grade.scv")
 print(ants.head())
 
 # create contingency table
-table = None
+table = pd.crosstab(ants.Grade, ants.Ant)
 print(table)
 
 # run Chi-Square test and print p-value
-
+chi2, pval, dof, expected = chi2_contingency(table)
+print(pval)
 
 # determine significance
-significant = None
+# pval < sign level then there is significant association (True)
+# pval > sign level then there is no significant association (False)
+significant = False
---
+++ Master_stats_with_Python/L8_hypothesis_testing/L8_9.py
@@ -0,0 +1,56 @@
+# project - Heart Disease Research Part II
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt 
+import seaborn as sns
+
+# load data
+heart = pd.read_csv('Codecademy\Master_stats_with_Python\L8_hypothesis_testing\heart_disease.csv')
+
+print(heart.head(5))
+
+#1
+
+
+#2
+
+
+
+#3
+
+
+
+#4
+
+
+#5
+
+
+#6
+
+
+#7
+
+
+#8
+
+
+#9
+
+
+#10
+
+
+#11
+
+
+#12
+
+
+#13
+
+
+#14
+
+
