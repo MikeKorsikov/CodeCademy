36e9236f145979fa85e52f037f4536045bc15aa9
MikeKorsikov <nikolay.korsikov@gmail.com>
2025-02-04 20:17:00 +0100

    Started simulation

---
+++ Master_stats_with_Python/L8_hypothesis_testing/L8_11.py
@@ -0,0 +1,13 @@
+# Simulation
+
+
+import pandas as pd
+from scipy.stats import chi2_contingency
+
+data = pd.read_csv("Codecademy\Master_stats_with_Python\L8_hypothesis_testing\ab_data.csv")
+print(data.head())
+
+# calculate contingency table here
+
+
+# run your chi square test here
