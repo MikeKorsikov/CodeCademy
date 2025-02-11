0aaa6e292620973943a0cdeb9fa25ebc033ce66c
MikeKorsikov <106008283+MikeKorsikov@users.noreply.github.com>
2025-02-11 13:50:15 +0100

    Started multiple LR

---
+++ Master_stats_with_Python/L9_linear_regression/L9_11.py
@@ -0,0 +1,9 @@
+# multiple linear regression
+
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+student = pd.read_csv('student.csv')
+
+# Add code for scatter plot here:
