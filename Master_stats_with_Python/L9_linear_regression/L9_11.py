07a024903bf77baf08411c7b7ba3186c7622cecd
MikeKorsikov <106008283+MikeKorsikov@users.noreply.github.com>
2025-02-12 13:08:38 +0100

    Update L9_11.py

--- Master_stats_with_Python/L9_linear_regression/L9_11.py
+++ Master_stats_with_Python/L9_linear_regression/L9_11.py
@@ -7,3 +7,5 @@ import matplotlib.pyplot as plt
 student = pd.read_csv('student.csv')
 
 # Add code for scatter plot here:
+sns.lmplot(x='math1', y='port3', hue='address', fit_reg=False, data=student)
+plt.show()
