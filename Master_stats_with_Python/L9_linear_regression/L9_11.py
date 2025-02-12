904a5c27404b60d28914a18db9f1ce9f7d6776dd
MikeKorsikov <106008283+MikeKorsikov@users.noreply.github.com>
2025-02-12 13:28:56 +0100

    Update L9_11.py

--- Master_stats_with_Python/L9_linear_regression/L9_11.py
+++ Master_stats_with_Python/L9_linear_regression/L9_11.py
@@ -9,3 +9,14 @@ student = pd.read_csv('student.csv')
 # Add code for scatter plot here:
 sns.lmplot(x='math1', y='port3', hue='address', fit_reg=False, data=student)
 plt.show()
+
+# Fit regression model here:
+model1 = sm.OLS.from_formula('port3 ~ math1 + address', data=student).fit()
+# Print intercept and coefficients here:
+print(model1.params)
+print(model1.summary())
+
+# Save intercept and coefficients here:
+b0 = model1.params[0] # intercept
+b1 = model1.params[2] # math
+b2 = model1.params[1] # address
