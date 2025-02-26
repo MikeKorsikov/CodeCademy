6372cde3f04ee70ca506f0da28ad5259db93f52c
MikeKorsikov <106008283+MikeKorsikov@users.noreply.github.com>
2025-02-26 13:14:09 +0100

    Create L2.py

---
+++ Stats_NumPy/L1/L2.py
@@ -0,0 +1,11 @@
+# Logical Operations with Arrays
+porridge = np.array([79, 65, 50, 63, 56, 90, 85, 98, 79, 51])
+
+cold = porridge[porridge < 60]
+print(cold)
+
+hot = porridge[porridge > 80]
+print(hot)
+
+just_right = porridge[(porridge >= 60) & (porridge <= 80)]
+print(just_right)
