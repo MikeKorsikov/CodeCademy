e35cf81377875138337c57cc84a08525a31e323d
MikeKorsikov <nikolay.korsikov@gmail.com>
2025-03-06 21:06:10 +0100

    added block and blockchain

---
+++ Blockchain/blockchain.py
@@ -0,0 +1,12 @@
+#imports the Block class from block.py
+from block import Block
+
+class Blockchain:
+    def __init__(self):
+        self.chain = []
+        self.all_transactions = []
+        self.genesis_block()
+    
+    def genesis_block(self):
+        genesis_block = Block([], "0")
+        self.chain.append(genesis_block)
