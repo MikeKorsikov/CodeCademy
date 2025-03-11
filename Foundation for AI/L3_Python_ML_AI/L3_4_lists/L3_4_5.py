94e68991c9e178085722389e29c4eb9431a6e041
MikeKorsikov <106008283+MikeKorsikov@users.noreply.github.com>
2025-03-11 16:50:58 +0100

    Started project on lists

---
+++ Foundation for AI/L3_Python_ML_AI/L3_4_lists/L3_4_5.py
@@ -0,0 +1,48 @@
+#Project on lists
+
+names = ["Mohamed", "Sara", "Xia", "Paul", "Valentina", "Jide", "Aaron", "Emily", "Nikita", "Paul"]
+insurance_costs = [13262.0, 4816.0, 6839.0, 5054.0, 14724.0, 5360.0, 7640.0, 6072.0, 2750.0, 12064.0]
+
+# Add your code here
+
+#1
+names.insert(len(names)+1, "Priscilla")
+insurance_costs.insert(len(names)+1, 8320.0)
+print(names)
+print(insurance_costs)
+
+
+#2
+
+
+#3
+
+
+#4
+
+
+#5
+
+
+#6
+
+
+#7
+
+
+#8
+
+
+#9
+
+
+#10
+
+
+#11
+
+
+#12
+
+
+#13
