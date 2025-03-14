dbb1256424d9350395baf17f837c99ce7f3295f5
MikeKorsikov <106008283+MikeKorsikov@users.noreply.github.com>
2025-03-14 12:48:18 +0100

    Update L3_5_1.py

--- Foundation for AI/L3_Python_ML_AI/L3_5_cycles/L3_5_1.py
+++ Foundation for AI/L3_Python_ML_AI/L3_5_cycles/L3_5_1.py
@@ -1,1 +1,10 @@
-#
+python_topics = ["variables", "control flow", "loops", "modules", "classes"]
+
+#Your code below: 
+length = len(python_topics)
+
+index = 0
+
+while index < length:
+  print(f"I am learning about{python_topics[index]}")
+  index +=1
