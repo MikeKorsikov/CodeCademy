b222a4602b3eadf2812769b54b232ed7a1bfd64d
MikeKorsikov <nikolay.korsikov@gmail.com>
2025-03-17 12:57:33 +0100

    started strip

---
+++ Foundation for AI/L4_Python_ML_AI_part_II/L4_2_strings/L4_2_2.py
@@ -0,0 +1,23 @@
+# splitting strings
+
+authors = "Audre Lorde,Gabriela Mistral,Jean Toomer,An Qi,Walt Whitman,Shel Silverstein,Carmen Boullosa,Kamala Suraiyya,Langston Hughes,Adrienne Rich,Nikki Giovanni"
+
+author_names = authors.split(",")
+print(author_names)
+
+author_last_names = [name.split()[-1] for name in author_names]
+print(author_last_names)
+
+# joining strings
+
+reapers_line_one_words = ["Black", "reapers", "with", "the", "sound", "of", "steel", "on", "stones"]
+
+reapers_line_one = ' '.join(reapers_line_one_words)
+print(reapers_line_one)
+
+#
+
+winter_trees_lines = ['All the complicated details', 'of the attiring and', 'the disattiring are completed!', 'A liquid moon', 'moves gently among', 'the long branches.', 'Thus having prepared their buds', 'against a sure winter', 'the wise trees', 'stand sleeping in the cold.']
+
+winter_trees_full = '\n'.join(winter_trees_lines)
+print(winter_trees_full)
---
+++ Foundation for AI/L4_Python_ML_AI_part_II/L4_2_strings/L4_2_3.py
@@ -0,0 +1,6 @@
+# strip
+
+featuring = "           rob thomas                 "
+print(featuring.strip())
+# => 'rob thomas'
+
