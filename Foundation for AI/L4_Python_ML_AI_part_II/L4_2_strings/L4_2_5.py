90439f8783659a067d0da37308946b3614031801
MikeKorsikov <106008283+MikeKorsikov@users.noreply.github.com>
2025-03-19 14:14:25 +0100

    completed project

--- Foundation for AI/L4_Python_ML_AI_part_II/L4_2_strings/L4_2_5.py
+++ Foundation for AI/L4_Python_ML_AI_part_II/L4_2_strings/L4_2_5.py
@@ -15,3 +15,83 @@ Lorena Hodson ,65, 33.1 , #19370.0;
 Isaac Vu ,34, 24.8,   #7045.0"""
 
 # Add your code here
+#1
+print(medical_data)
+
+#2
+updated_medical_data = medical_data.replace("#", "$")
+print(updated_medical_data)
+
+#3
+num_records  = 0
+
+#4
+for i in updated_medical_data:
+  if i == "$":
+    num_records += 1
+
+#5
+print(f"There are {num_records} medical records in the data.")
+
+#6
+medical_data_split = updated_medical_data.split(";")
+print(medical_data_split)
+
+#7
+medical_records = []
+
+#8
+for i in medical_data_split:
+  medical_records.append(i.split(','))
+
+print(medical_records)
+
+#9
+medical_records_clean = []
+
+#10-12
+for record in medical_records:
+  record_clean = []
+  for item in record:
+    record_clean.append(item.strip())
+  medical_records_clean.append(record_clean)
+
+#13
+print(medical_records_clean)
+
+#14-15
+for record in medical_records_clean:
+  record[0] = record[0].upper()
+  print(record[0])
+
+#16
+names = []
+ages = []
+bmis = []
+insurance_costs = []
+
+#17
+for record in medical_records_clean:
+  names.append(record[0])
+  ages.append(record[1])
+  bmis.append(record[2])
+  insurance_costs.append(record[3])
+
+#18
+print("Names: " + str(names))
+print("Ages: " + str(ages))
+print("BMI: "  + str(bmis))
+print("Insurance Costs: " + str(insurance_costs))
+
+#19
+total_bmi = 0
+
+#20
+for bmi in bmis:
+  total_bmi += float(bmi)
+print(total_bmi)
+
+#21
+average_bmi = total_bmi / len(bmis)
+print(f"Average BMI: {average_bmi}")
+
