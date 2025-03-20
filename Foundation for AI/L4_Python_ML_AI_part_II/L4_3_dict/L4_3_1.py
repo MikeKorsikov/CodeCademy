85a5b919117d58eabe0402fe541306c32d1c21f1
MikeKorsikov <106008283+MikeKorsikov@users.noreply.github.com>
2025-03-20 13:41:47 +0100

    Update L4_3_1.py

--- Foundation for AI/L4_Python_ML_AI_part_II/L4_3_dict/L4_3_1.py
+++ Foundation for AI/L4_Python_ML_AI_part_II/L4_3_dict/L4_3_1.py
@@ -1,2 +1,16 @@
 # dictionaries
 
+user_ids = {"teraCoder": 9018293, "proProgrammer": 119238}
+
+user_ids.update({"theLooper":138475, "stringQueen": 85739})
+
+print(user_ids)
+
+# Dict Comprehensions
+drinks = ["espresso", "chai", "decaf", "drip"]
+caffeine = [64, 40, 0, 120]
+
+zipped_drinks = zip(drinks, caffeine)
+drinks_to_caffeine  = {key:value for key, value in zipped_drinks}
+ 
+print(drinks_to_caffeine)
