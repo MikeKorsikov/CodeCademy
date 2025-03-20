ffd6facc8fe03779a3c642631956706ae99ab835
MikeKorsikov <106008283+MikeKorsikov@users.noreply.github.com>
2025-03-20 14:17:54 +0100

    Create L4_3_2.py

---
+++ Foundation for AI/L4_Python_ML_AI_part_II/L4_3_dict/L4_3_2.py
@@ -0,0 +1,36 @@
+zodiac_elements = {"water": ["Cancer", "Scorpio", "Pisces"], "fire": ["Aries", "Leo", "Sagittarius"], "earth": ["Taurus", "Virgo", "Capricorn"], "air":["Gemini", "Libra", "Aquarius"]}
+
+zodiac_elements["energy"] = "Not a Zodiac element"
+
+if "energy" in zodiac_elements:
+  print(zodiac_elements["energy"])
+
+# get method
+user_ids = {"teraCoder": 100019, "pythonGuy": 182921, "samTheJavaMaam": 123112, "lyleLoop": 102931, "keysmithKeith": 129384}
+
+tc_id = user_ids.get("teraCoder", 0)
+print(tc_id)
+
+# keys
+user_ids = {"teraCoder": 100019, "pythonGuy": 182921, "samTheJavaMaam": 123112, "lyleLoop": 102931, "keysmithKeith": 129384}
+num_exercises = {"functions": 10, "syntax": 13, "control flow": 15, "loops": 22, "lists": 19, "classes": 18, "dictionaries": 18}
+
+users = user_ids.keys()
+print(users)
+
+lessons = num_exercises.keys()
+print(lessons)
+
+# values
+num_exercises = {"functions": 10, "syntax": 13, "control flow": 15, "loops": 22, "lists": 19, "classes": 18, "dictionaries": 18}
+total_exercises = 0
+for exercise in num_exercises.values():
+ total_exercises +=exercise
+
+print(total_exercises)
+
+# items
+pct_women_in_occupation = {"CEO": 28, "Engineering Manager": 9, "Pharmacist": 58, "Physician": 40, "Lawyer": 37, "Aerospace Engineer": 9}
+
+for key, value in pct_women_in_occupation.items():
+ print(f"Women make up {value} percent of {key}s.")
