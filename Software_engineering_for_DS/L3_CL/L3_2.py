f8bdcc65009399f75c2c38cacf2fb8de8c37a404
MikeKorsikov <nikolay.korsikov@gmail.com>
2025-04-14 21:11:48 +0200

    Completed project on CL

--- Software_engineering_for_DS/L3_CL/L3_2.py
+++ Software_engineering_for_DS/L3_CL/L3_2.py
@@ -1,1 +1,4 @@
 # Viewing and changing the file system
+# ls -a  lists all contents, including hidden files and directories
+# ls -l lists all contents of a directory in long format, as well as the file permissions
+# ls -t orders files and directories by the time they were last modified
