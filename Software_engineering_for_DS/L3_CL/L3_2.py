6a0903036b038a52a3f941fe0897a89fbdc8a32d
MikeKorsikov <106008283+MikeKorsikov@users.noreply.github.com>
2025-04-15 16:59:07 +0200

    Completed viewing and editing

--- Software_engineering_for_DS/L3_CL/L3_2.py
+++ Software_engineering_for_DS/L3_CL/L3_2.py
@@ -2,3 +2,9 @@
 # ls -a  lists all contents, including hidden files and directories
 # ls -l lists all contents of a directory in long format, as well as the file permissions
 # ls -t orders files and directories by the time they were last modified
+
+# cat file.txt -> read content of file
+# cp source.txt destination.txt -> copy file
+# * -> wildecard to copy multiple files
+# mv file.txt -> move file
+# rm -> remove files
