abeeb99b0aa00c905e936bf145321c0b9d241b97
MikeKorsikov <106008283+MikeKorsikov@users.noreply.github.com>
2025-04-17 15:03:57 +0200

    Update L3_4.py

--- Software_engineering_for_DS/L3_CL/L3_4.py
+++ Software_engineering_for_DS/L3_CL/L3_4.py
@@ -1,2 +1,17 @@
 # Configuring the Command Line Environment
 
+# nano file.txt -> opens the specified file in the nano text editor
+
+# Ctrl O -> save file output
+# Ctrl X -> exit nano editor
+# Ctrl G -> open help menu
+
+# .bash_profile - file used to store environment settings for terminal
+# alias word="command" -> allows you to create keyboard shortcuts, or aliases, for commonly used commands (done in nano)
+# source .bash_profile -> to apply settings to current sessions
+# export USER="Jane Doe" -> makes the variable available to all child sessions (done in nano)
+
+# echo $HOME -> 
+# echo $PATH -> lists which directories contain scripts
+# env ->returns a list of the environment variables for the current user
+
