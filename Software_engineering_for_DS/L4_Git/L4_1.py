f9a5b67614e4284dcb11a2e2f7a6e04b700168d9
MikeKorsikov <nikolay.korsikov@gmail.com>
2025-04-19 20:55:11 +0200

    Started GitHub

--- Software_engineering_for_DS/L4_Git/L4_1.py
+++ Software_engineering_for_DS/L4_Git/L4_1.py
@@ -1,1 +1,18 @@
 #GIT & GitHub
+
+# git init - initialize a new Git repository 
+# git status - check the status of the repository
+# git add filename - add files to the staging area
+
+# git diff filename - show changes between the working directory and the staging area
+# git commit -m "message" - commit changes to the repository
+# git log - show the commit history
+# git log --oneline - show a simplified commit history
+# git log --oneline --graph - --graph shows the commit history as a graph
+
+# git show HEAD - show the latest commit
+# git checkout HEAD filename - revert changes to a file in the working directory
+# git reset HEAD filename - unstage a file in the staging area 
+
+# git stash - save changes in the working directory and staging area temporarily
+# git stash pop - apply the changes saved in the stash to the working directory
