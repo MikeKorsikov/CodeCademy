177a6c1307e32d0361527e9973a23de18257a56a
MikeKorsikov <nikolay.korsikov@gmail.com>
2025-04-20 19:53:02 +0200

    Started exceptions

---
+++ Software_engineering_for_DS/L5_SE_in_P_II/L5_1.py
@@ -0,0 +1,12 @@
+# |Unit testing
+# Exceptions
+sale_instruments = ['Violin', 'Conga', 'Clavinet']
+
+print('The following ' + str(len(sale_instruments)) + ' instruments are on sale:')
+print(sale_instruments[0])
+print(sale_instruments[1])
+print(sale_instruments[2])
+print(TypeError.__bases__)
+
+###
+
