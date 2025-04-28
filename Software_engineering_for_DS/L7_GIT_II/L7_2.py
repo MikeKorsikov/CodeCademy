97eca390fa9af073c9fb017682884c3c81407076
MikeKorsikov <106008283+MikeKorsikov@users.noreply.github.com>
2025-04-28 15:58:39 +0200

    Finished git branching

--- Software_engineering_for_DS/L7_GIT_II/L7_2.py
+++ Software_engineering_for_DS/L7_GIT_II/L7_2.py
@@ -4,3 +4,6 @@
 # git branch new_branch - creates a new branch
 # git checkout branch_name - switches to the specified branch 
 # git merge branch_name - merges the specified branch into the current branch
+# git branch -d branch_name - to delete branch
+
+
