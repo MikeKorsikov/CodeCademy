d902046635720146faad0ac7e1eee3b69a2215c4
MikeKorsikov <106008283+MikeKorsikov@users.noreply.github.com>
2025-04-29 10:42:03 +0200

    Finished git teamwork (fetch-merge-push)

--- Software_engineering_for_DS/L7_GIT_II/L7_3.py
+++ Software_engineering_for_DS/L7_GIT_II/L7_3.py
@@ -1,4 +1,7 @@
 # git teamwork
 
-# git clone remote_location clone_name
-
+# git clone remote_location clone_name -> Creates a local copy of a remote
+# git remote -v ->  Lists a Git project’s remotes
+# git fetch -> Fetches work from the remote into the local copy
+# git merge origin/master -> Merges origin/master into your local branch
+# git push origin <your_branch_name> -> Pushes a local branch to the origin remote
